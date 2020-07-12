2ec115c5dea2804aefa7b7022678119e85567a40
felipeCordeiroDosSantos <felipecordeirodosantos@gmail.com>
2020-07-12 13:07:46 -0300

    ajustes no codigo

--- controller/taskController.js
+++ controller/taskController.js
@@ -1,23 +1,31 @@
 const db = require('../modules/db')
 const { change } = require('../modules/db')
 
+function addTask(req, res) {
+    let newTask = req.body.newTask
+    db.addTask(newTask)
+}
+
+function updateTask(req, res) {
+    let newTask = req.body.newTask
+    let index = req.body.index
+    db.updateTask(index, newTask)
+}
+
+function deleteTask(req, res) {
+    let index = req.body.index
+    db.deleteTask(index)
+}
+
+function changeTask(req, res) {
+    let newTask = req.body.newTask
+    let index = req.body.index
+    db.changeTask(index, newTask)
+}
+
 module.exports = {
-    addTask: function(req, res) {
-        let newTask = req.body.newTask
-        db.addTask(newTask)
-    },
-    updateTask: function(req, res) {
-        let newTask = req.body.newTask
-        let index = req.body.index
-        db.updateTask(index, newTask)
-    },
-    deleteTask: function(req, res) {
-        let index = req.body.index
-        db.deleteTask(index)
-    },
-    changeTask: function(req, res) {
-        let newTask = req.body.newTask
-        let index = req.body.index
-        db.changeTask(index, newTask)
-    }
+    addTask,
+    updateTask,
+    deleteTask,
+    changeTask
 }
